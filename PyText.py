4011b2a6fb5d6eb48af87f93966a413940c0f7d3
Sunlightsunglasses <146563486+Sunlightsunglasses@users.noreply.github.com>
2024-07-22 18:25:41 +0700

    PyText.py was made
    18:25 Jul 22nd 2024

---
+++ PyText.py
@@ -0,0 +1,19 @@
+import sys
+
+class text:
+	#font
+	B = "\033[1m"
+	I = "\033[3m"
+	U = "\033[4m"
+	#text color""\033[38;5;{ID}m""
+	red = "\033[38;5;1m"
+	green = "\033[38;5;2m"
+	yellow = "\033[38;5;3m"
+	blue = "\033[38;5;4m"
+	cyan = "\033[38;5;6m"
+	purple = "\033[38;5;5m"
+	gray = "\033[38;5;7m"
+	white = "\033[38;5;15m"
+	black = "\033[38;5;0m"
+	#end
+	end = "\033[0m"
